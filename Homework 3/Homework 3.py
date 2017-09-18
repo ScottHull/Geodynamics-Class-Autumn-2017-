aac80443fc50b540fece28c70caa02ba9a1f8eef
Scott Hull <ScottHull@users.noreply.github.com>
2017-09-18 12:27:05 -0400

    Add files via upload

--- Homework 3/Homework 3.py
+++ Homework 3/Homework 3.py
@@ -65,8 +65,6 @@ def heat_as_time(H, halflife, time):
         H_dict.append(H)
     return H_dict
 
-print(heat_as_time(H=2.92*10**-5, halflife=1.25*10**9, time=timespan))
-
 plt.plot(timespan, heat_as_time(H=2.92*10**-5, halflife=1.25*10**9, time=timespan), label='K in core')
 plt.title("Homework 3, Question 1d")
 plt.xlabel('Time (Yr)')
@@ -84,19 +82,18 @@ plt.close()
 if "hw3_prob3.csv" in os.listdir(os.getcwd()):
     os.remove("hw3_prob3.csv")
 
-deltaX = 0.01
-Kappa = 0.000005
-deltaT = (0.2 * deltaX**2) / Kappa
-boundary_T = 1800
-slab_T = 300
-print("Delta T is: {}".format(deltaT))
-max_time_interations = 250
+Kappa = 0.000005 * 3.154*10**7 * 1*10**-6 # convert seconds to years, m^2 to km^2
+deltaX = 1 # km
+deltaTime = (0.2 * deltaX**2) / Kappa # years
+boundary_T = 1800 # degK
+slab_T = 300 # degK
+slab_thickness = 100
+max_time_interations = 25000
 curr_time_iteration = 1
-curr_depth = 0
-start_temp = 25 + 273.15
-print(deltaT)
+print("\nModel Parameters:\nKappa: {} km^2/yr\ndeltaTime: {} years\ndeltaX: {} km\nSlab Thickness: {} km\nMax model iterations: {} ({} billion years)".format(
+    Kappa, deltaTime, deltaX, slab_thickness, max_time_interations, (max_time_interations*deltaTime) / (1*10**9)))
 
-df = pd.DataFrame({'Depth': [i for i in list(range(11))], "Initial Condition": [slab_T for i in list(range(11))]})
+df = pd.DataFrame({'Depth': [i for i in list(range(slab_thickness + 1))], "Initial Condition": [slab_T for i in list(range(slab_thickness + 1))]})
 for i in list(range(max_time_interations)):
     time = str(curr_time_iteration)
     prev_time_iteration = str(curr_time_iteration - 1)
@@ -105,8 +102,8 @@ for i in list(range(max_time_interations)):
     if i != max_time_interations + 1:
         if curr_time_iteration != 1:
             vals.append(boundary_T)
-            for row in df['Depth'].ix[1:9]:
-                T = (Kappa*deltaT/deltaX**2)*(df[prev_time_iteration][row + 1] + df[prev_time_iteration][row - 1]
+            for row in df['Depth'].ix[1:slab_thickness - 2]:
+                T = (Kappa*deltaTime/deltaX**2)*(df[prev_time_iteration][row + 1] + df[prev_time_iteration][row - 1]
                                               -2*df[str(prev_time_iteration)][row]) + df[str(prev_time_iteration)][row]
                 vals.append(T)
             vals.append(boundary_T)
@@ -116,31 +113,32 @@ for i in list(range(max_time_interations)):
             df[time] = ''
             df2 = pd.DataFrame({time: []})
             for row in df.index:
-                T = (Kappa * deltaT / deltaX ** 2) * (slab_T + slab_T - 2 * slab_T) + slab_T
+                T = (Kappa * deltaTime / deltaX ** 2) * (slab_T + slab_T - 2 * slab_T) + slab_T
                 vals.append(T)
         df2 = pd.DataFrame({time: vals})
         df[time] = df2[time]
-        curr_depth += 1
         curr_time_iteration += 1
         boundary_T += 0
-        print("New iteration: {}".format(curr_time_iteration))
+        # print("New iteration: {}".format(curr_time_iteration))
     else:
         curr_time_iteration += 1
 
-df.to_csv("hw3_prob3.csv")
+# df.to_csv("hw3_prob3.csv")
 
 df2 = df
 df2['Depth'] = df2.index
 plt.Figure()
-new = df2.iloc[0:11, 3:]
+new = df2.iloc[0:slab_thickness + 1, 3:]
 temp = []
 for row in new.iterrows():
     index, data = row
     temp.append(data.tolist())
 for index, i in enumerate(temp):
-    plt.plot(list(range(len(i))), i, label=str(index) + 'km in slab')
+    if index != 0:
+        if slab_thickness % index == 0:
+            plt.plot(list(range(len(i))), i, label=str(index) + 'km in slab')
 plt.grid()
-plt.xlabel("Model Iterations (1 iteration = 4 years)")
+plt.xlabel("Model Iterations (1 iteration = {} years)".format(round(deltaTime, 2)))
 plt.ylabel('Slab temperature (degK)')
 plt.title("HW 3 Problem 3")
 plt.legend(loc='lower right')
